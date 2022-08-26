cec8d308079a2a5e9680c0a79f02ca8346f22d43
menghuiguli <fujia0@qq.com>
2022-08-26 17:28:36 +0800

    router link and routes

--- src/App.js
+++ src/App.js
@@ -14,15 +14,29 @@ import {
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Router>
+      
+      <ul className="App-header">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
+      
         <Routes>
         <Route exact path="/" element={<Home/>}></Route>
         <Route exact path="/about" element={<About />}></Route>
         <Route exact path="/contact" element={<ContactUs />}></Route>
         </Routes>
-      </Router>
+      
     </div>
+    </Router>
   );
 }
 
